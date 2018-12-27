4b510b0a932395205548a54de542e8ba4a344b86
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-12-26 20:16:44 -0500

    create winecard

--- wine-shop-client/src/components/WineCard.js
+++ wine-shop-client/src/components/WineCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WineCard = ({ wine }) => (
   <div key={wine.id} className="WineCard">
     <h3>{wine.name}</h3>
-    <p>Year: ${wine.year}</p>
+    <p>Year: {wine.year}</p>
     <p>Price: ${wine.price}</p>
     <img className="WineImage" src={wine.img_url} alt={wine.name} />
   </div>
