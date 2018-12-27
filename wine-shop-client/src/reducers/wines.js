d58030ff9239c40087e365f174446af243865d9c
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-12-27 12:24:21 -0500

    work on reducers

---
+++ wine-shop-client/src/reducers/wines.js
@@ -0,0 +1,12 @@
+export default (state = [], action) => {
+  switch(action.type) {
+    case 'GET_WINES_SUCCESS':
+      return action.wines;
+
+    case 'CREATE_WINE_SUCCESS':
+      return state.concat(action.wine);
+
+    default:
+      return state;
+  }
+}
