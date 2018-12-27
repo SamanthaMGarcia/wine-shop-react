e960b26005f4b5e3b47bc4f6a27274efe6fd0d0e
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-12-26 19:10:08 -0500

    create App.js

---
+++ wine-shop-client/src/containers/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Wines from './Wines';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Wines />
+      </div>
+    );
+  }
+}
+
+export default App;
