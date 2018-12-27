98b96452a200b2f3d220c19a50909a3d6e1ec55d
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-12-26 19:44:06 -0500

    add css

--- wine-shop-client/src/components/WineCard.js
+++ wine-shop-client/src/components/WineCard.js
@@ -4,7 +4,7 @@ const WineCard = ({ wine }) => (
   <div key={wine.id} className="WineCard">
     <h3>{wine.name}</h3>
     <p>Year: ${wine.year}</p>
-    <p>Price: ${surfboard.price}</p>
+    <p>Price: ${wine.price}</p>
     <img className="WineImage" src={wine.img_url} alt={wine.name} />
   </div>
 )
---
+++ wine-shop-client/src/containers/Wines.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import WineCard from '../components/WineCard';
+import WineForm from './WineForm';
+import { getWines } from '../actions/wines';
+import './Wines.css';
+
+class Wines extends Component {
+
+  componentDidMount() {
+    this.props.getWines()
+  }
+
+  render() {
+    return (
+      <div className="WinesContainer">
+        <h1>Wines</h1>
+        {this.props.wines.map(wine => <WineCard key={wine.id} wine={wine} />)}
+        <WineForm />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return ({
+    wines: state.wines
+  })
+}
+
+ export default connect(mapStateToProps, { getWines })(Wines);
